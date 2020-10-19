6f5169e47b4d079f451f16edff3152d80fa717e1
Michael Bukachi <michaelbukachi@gmail.com>
2020-10-19 03:02:25 +0300

    test: Add requests_mock

--- ussd/tests/test_http_screen.py
+++ ussd/tests/test_http_screen.py
@@ -1,6 +1,8 @@
 from unittest import mock
+
+import requests_mock
+
 from ussd.tests import UssdTestCase
-from ussd.tests.utils import MockResponse
 
 
 class TestHttpScreen(UssdTestCase.BaseUssdTestCase):
@@ -21,79 +23,59 @@ class TestHttpScreen(UssdTestCase.BaseUssdTestCase):
         )
     )
 
-    @mock.patch("ussd.core.requests.request")
-    def test(self, mock_request):
-        mock_response = MockResponse({"balance": 250})
-        mock_request.return_value = mock_response
-        ussd_client = self.ussd_client()
+    def test(self):
+        with requests_mock.Mocker() as m:
+            m.get(requests_mock.ANY, json={"balance": 250})
+            m.post(requests_mock.ANY, json={"balance": 250})
+            ussd_client = self.ussd_client()
 
-        self.assertEqual(
-            "Testing response is being saved in "
-            "session status code is 200 and "
-            "balance is 250 and full content {'balance': 250}.\n",
-            ussd_client.send('')
-        )
-        expected_calls = [
-            mock.call(
-                method='get',
-                url="http://localhost:8000/mock/balance",
-                params=dict(
-                    phone_number="200",
-                    session_id=ussd_client.session_id
-                ),
-                verify=False,
-                headers={"content-type": "application/json",
-                         "user-agent": "my-app/0.0.1"}
-            ),
-            mock.call(
-                method="get",
-                url="http://localhost:8000/mock/balance/200/"
-            ),
-            mock.call(
-                method='post',
-                url='http://localhost:8000/mock/balance',
-                params=dict(
-                    phone_numbers=[200, 201, 202],
-                    session_id=ussd_client.session_id
-                ),
-                verify=True,
-                timeout=30,
-                headers={"content-type": "application/json"}
+            self.assertEqual(
+                "Testing response is being saved in "
+                "session status code is 200 and "
+                "balance is 250 and full content {'balance': 250}.\n",
+                ussd_client.send('')
             )
-        ]
-        # test requests that were made
-        mock_request.assert_has_calls(expected_calls)
+            history = m.request_history
+            assert history[0].method == 'GET'
+            assert history[0].qs == {'phone_number': ['200'], 'session_id': [ussd_client.session_id]}
+            assert not history[0].verify
 
-    # @mock.patch("ussd.screens.http_screen.http_task.delay")
-    # @mock.patch("ussd.tasks.requests.request")
-    # def test_async_workflow(self, mock_request, mock_http_task):
-    #     mock_response = MockResponse({"balance": 257})
-    #     mock_request.return_value = mock_response
-    #
-    #     ussd_client = self.ussd_client()
-    #     ussd_client.send('')
-    #
-    #     # check http_task is called
-    #     mock_http_task.assert_called_once_with(
-    #         request_conf=dict(
-    #             method='get',
-    #             url="https://localhost:8000/mock/submission",
-    #             params={'phone_number': '200',
-    #                     'session_id': ussd_client.session_id}
-    #         )
-    #     )
+            assert history[1].method == 'GET'
 
-    @mock.patch("ussd.core.requests.request")
-    def test_json_decoding(self, mock_request):
-        mock_response = MockResponse("Balance is 257")
-        mock_request.return_value = mock_response
+            assert history[2].method == 'POST'
+            assert history[2].qs == {'phone_numbers': ['200', '201', '202'], 'session_id': [ussd_client.session_id]}
+            assert history[2].verify
 
-        ussd_client = self.ussd_client()
-        ussd_client.send('')
+    @mock.patch("ussd.screens.http_screen.http_task.delay")
+    def test_async_workflow(self, mock_http_task):
+        with requests_mock.Mocker() as m:
+            m.get(requests_mock.ANY, json={"balance": 250})
+            m.post(requests_mock.ANY, json={"balance": 250})
 
-        self.assertEqual(
-            "Testing response is being saved in "
-            "session status code is 200 and "
-            "balance is  and full content Balance is 257.\n",
+            ussd_client = self.ussd_client()
             ussd_client.send('')
-        )
+
+            # check http_task is called
+            mock_http_task.assert_called_once_with(
+                request_conf=dict(
+                    method='get',
+                    url="https://localhost:8000/mock/submission",
+                    params={'phone_number': '200',
+                            'session_id': ussd_client.session_id}
+                )
+            )
+
+    def test_json_decoding(self):
+        with requests_mock.Mocker() as m:
+            m.get(requests_mock.ANY, text='Balance is 257')
+            m.post(requests_mock.ANY, text='Balance is 257')
+
+            ussd_client = self.ussd_client()
+            ussd_client.send('')
+
+            self.assertEqual(
+                "Testing response is being saved in "
+                "session status code is 200 and "
+                "balance is  and full content Balance is 257.\n",
+                ussd_client.send('')
+            )
